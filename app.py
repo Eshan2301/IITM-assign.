de07428f776f89612ea1aa517188599b5877cf7c
Eshan2301 <119496927+Eshan2301@users.noreply.github.com>
2022-11-30 21:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ def main():
   
 
 
-st.title("Greater Of Three Numbers")
-a=st.number_input("Enter First Number")
-b=st.number_input("Enter Second Number")
-c=st.number_input("Enter Third Number")
+  st.title("Greater Of Three Numbers")
+  a=st.number_input("Enter First Number")
+  b=st.number_input("Enter Second Number")
+  c=st.number_input("Enter Third Number")
 
-if(a>b) and (a>c):
-  st.write("a is greater")
-elif(b>a) and (b>c):
-  st.write("b is greater")
-else:
-  st.write("c is greater")
+  if(a>b) and (a>c):
+    st.write("a is greater")
+  elif(b>a) and (b>c):
+    st.write("b is greater")
+  else:
+    st.write("c is greater")
   
 
   
